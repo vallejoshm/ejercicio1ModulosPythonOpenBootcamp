b7e58620f611d1b9d1f317e6ecae9af9509739f5
Hernan Vallejos <vallejoshm@gmail.com>
2022-08-31 19:20:37 -0300

    primer commit

---
+++ main.py
@@ -0,0 +1,10 @@
+
+import calculadora.calculadora
+
+print(calculadora.calculadora.sumar(5,5))
+
+print(calculadora.calculadora.restar(5,5))
+
+print(calculadora.calculadora.multiplicar(5,5))
+
+print(calculadora.calculadora.dividir(5,5))
